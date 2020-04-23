d4bea8b3cdf6381b23885268f0e86b0de27ec3a8
Niraj Kumar <nirajkvinit@gmail.com>
2020-04-23 20:32:59 +0530

    Initial commit

---
+++ src/categoryList.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+interface Props {
+  selected: string;
+  categories: string[];
+  selectCategory: (category: string) => void;
+}
+
+export class CategoryList extends Component<Props> {
+  render() {
+    return (
+      <div>
+        {['All', ...this.props.categories].map((c) => {
+          console.log('-----', c);
+          let btnClass =
+            this.props.selected === c ? 'btn-primary' : 'btn-secondary';
+          return (
+            <button
+              key={`${c}-cat`}
+              className={`btn btn-block ${btnClass}`}
+              onClick={() => this.props.selectCategory(c)}
+            >
+              {c}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+}
